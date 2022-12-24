cb1aae52c3819badf4688f392e89ff3446919afa
zshijie <zshijie7910@gmail.com>
2022-12-24 16:53:46 +0800

    feat：解决空手右键移除高尔夫球时的延迟和give两个item的问题

--- src/main/java/net/fabricmc/mygolf/RegisterEvents.java
+++ src/main/java/net/fabricmc/mygolf/RegisterEvents.java
@@ -4,8 +4,10 @@ import com.jme3.math.Vector3f;
 import dev.lazurite.rayon.impl.bullet.thread.PhysicsThread;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.fabricmc.mygolf.entity.GolfBallEntity;
+import net.fabricmc.mygolf.items.GolfBall;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -24,22 +26,24 @@ public class RegisterEvents {
         {
             /* 手动的旁观者检查是必要的，因为 AttackBlockCallbacks 会在旁观者检查之前应用 */
             //敲打高尔夫球 远低近高
-            if(entity instanceof GolfBallEntity && !player.isSpectator()) {
+            if (entity instanceof GolfBallEntity && !player.isSpectator()) {
                 //根据玩家与球的位置计算
                 Vec3d ballPos = entity.getPos();
                 Vec3d playerPos = player.getPos();
                 Vec3d posDelta3d = ballPos.subtract(playerPos);
                 //Vector3f posDelta3f = new Vector3f((float) posDelta3d.x,(float) posDelta3d.y,(float) posDelta3d.z);
-                Vec2f posDelta2f = new Vec2f((float) posDelta3d.x,(float) posDelta3d.z); //水平方向向量差
+                Vec2f posDelta2f = new Vec2f((float) posDelta3d.x, (float) posDelta3d.z); //水平方向向量差
                 float hitDistance = posDelta2f.length(); //向量模
                 Vec2f hitDirection = posDelta2f.normalize(); //单位向量
-                float hitPitch = (30-hitDistance)/5;
+                float hitPitch = (30 - hitDistance) / 5;
                 Vector3f impulse = new Vector3f(hitDirection.x * 5, hitPitch, hitDirection.y * 5);
                 //rayon:给球一个冲量
                 PhysicsThread
                         .get(world.getServer())
-                        .execute(()->{((GolfBallEntity) entity).getRigidBody().applyCentralImpulse(impulse);});
-                if(world.isClient)
+                        .execute(() -> {
+                            ((GolfBallEntity) entity).getRigidBody().applyCentralImpulse(impulse);
+                        });
+                if (world.isClient)
                     System.out.println("imp " + impulse);
             }
             return ActionResult.PASS;
@@ -48,10 +52,11 @@ public class RegisterEvents {
         //事件：右击实体
         UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) ->
         {
-            //捡起高尔夫球
-            if(entity instanceof GolfBallEntity && !player.isSpectator() && player.getMainHandStack().isEmpty()) {
+            //高尔夫球没被移除时，捡起高尔夫球
+            if (entity instanceof GolfBallEntity && entity.isAlive() && !player.isSpectator() && player.getMainHandStack().isEmpty()) {
+                entity.discard();
                 player.giveItemStack(new ItemStack(RegisterItems.GOLF_BALL));
-                entity.kill();
+                return ActionResult.SUCCESS;
             }
             return ActionResult.PASS;
         });
@@ -71,12 +76,16 @@ public class RegisterEvents {
                 //玩家是否看向高尔夫球
                 if (entity instanceof GolfBallEntity) {
                     //渲染箭头
-                    if (!isPrevLookBall) {client.player.sendMessage(Text.of("look at ball"), true);}
+                    if (!isPrevLookBall) {
+                        client.player.sendMessage(Text.of("look at ball"), true);
+                    }
                     isPrevLookBall = true;
                     return;
                 }
             }
-            if (isPrevLookBall) {client.player.sendMessage(Text.of("look away ball"), true);}
+            if (isPrevLookBall) {
+                client.player.sendMessage(Text.of("look away ball"), true);
+            }
             isPrevLookBall = false;
         });
     }
